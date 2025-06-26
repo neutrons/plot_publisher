951d006991b3e4a270ff219554562641383615ef
Darsh <ddarsh5544@gmail.com>
2025-06-26 12:41:23 -0400

    Some more fixes based on feedback.

--- src/plot_publisher/__init__.py
+++ src/plot_publisher/__init__.py
@@ -5,6 +5,6 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
-from .plot_publisher import publish_plot, plot1d, plot_heatmap
+from ._plot_publisher import plot1d, plot_heatmap, publish_plot
 
-__all__ = ["publish_plot", "plot1d", "plot_heatmap", "__version__"] 
+__all__ = ["plot1d", "plot_heatmap", "publish_plot", "__version__"] 
